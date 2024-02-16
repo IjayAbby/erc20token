01f9a470c0ccdd376c7a82ba7bd3d559af3fcf29
Ijay Abby <inyangasi4@gmail.com>
2024-02-16 13:34:54 +0100

    Interface, myToken and ERC20 Token

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -1,62 +1,61 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.20;
+import "./Interfaces/IERC20.sol";
 
-contract ERC20Token {
-    string public name;
-    string public  symbol;
-    uint8 public immutable decimals;
-    uint256 public immutable totalSupply;
-    
-    mapping(address => uint256) _balances;
-    mapping(address => mapping (address => uint256)) _allowances;
+contract SaveERC20 {
+    address savingToken;
+    address owner;
 
-    event Transfer(address indexed  _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256);
+    mapping(address => uint256) savings;
 
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
-        name = _name;
-        symbol = _symbol;
-        decimals = 18;
-        totalSupply = _totalSupply;
-        _balances[msg.sender] = _totalSupply;
-    
+    event SavingSuccessful(address sender, uint256 amount);
+    event WithdrawSuccessful(address receiver, uint256 amount);
+
+    constructor(address _savingToken) {
+        savingToken = _savingToken;
+        owner = msg.sender;
     }
 
-    function balanceOf(address _owner) public view returns(uint256) {
-        require(_owner != address(0), "ERC20Token: Address is zero");
-         return _balances[_owner];
+    function deposit(uint256 _amount) external {
+        require(msg.sender != address(0), "address zero detected");
+        require(_amount > 0, "can't save zero value");
+        require(IERC20(savingToken).balanceOf(msg.sender) >= _amount, "not enough token");
+
+        require(IERC20(savingToken).transferFrom(msg.sender, address(this), _amount), "failed to transfer");
+
+        savings[msg.sender] += _amount;
+
+        emit SavingSuccessful(msg.sender, _amount);
+
     }
 
-    function transfer(address _to,uint256 _value) public returns (bool) {
-        uint transferFee = _value / 10;
-        require((_balances[msg.sender] >= _value) && (_balances[msg.sender] > 0), "Insufficient tokens");
-        _balances[msg.sender] -= _value + transferFee;
-        _balances[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
-        return true;
+    function withdraw(uint256 _amount) external {
+        require(msg.sender != address(0), "address zero detected");
+        require(_amount > 0, "can't withdraw zero value");
+
+        uint256 _userSaving = savings[msg.sender];
+
+        require(_userSaving >= _amount, "insufficient funds");
+
+        savings[msg.sender] -= _amount;
+
+        require(IERC20(savingToken).transfer(msg.sender, _amount), "failed to withdraw");
+
+        emit WithdrawSuccessful(msg.sender, _amount);
     }
 
-    function transferFrom(address  _from,address _to,uint256 _value) public returns (bool){
-        uint transferFee = _value / 10;
-        require(_allowances[msg.sender][_from] >= _value,"Not enough allowance");
-        require((_balances[_from] >= _value) && (_balances[_from] > 0), "Not  enough token in the owner's account");
-        _balances[_from] -= _value + transferFee;
-        _balances[_to] += _value;
-        _allowances[msg.sender][_from] -= _value;
-        emit Transfer(_from, _to, _value);
-        return true;
+    function checkUserBalance(address _user) external view returns (uint256) {
+        return savings[_user];
     }
 
-    function approve(address _spender, uint256 _value) public returns(bool) {
-        require(_balances[msg.sender] >= _value, "Insufficient tokens");
-        _allowances[_spender][msg.sender]  = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
+    function checkContractBalance() external view returns(uint256) {
+        return IERC20(savingToken).balanceOf(address(this));
     }
 
-    function allowance(address _owner, address _spender) public view returns(uint256) {
-        return _allowances[_owner][_spender];
-    }
+    function ownerWithdraw(uint256 _amount) external {
+        require(msg.sender == owner, "not owner");
 
+        IERC20(savingToken).transfer(msg.sender, _amount);
+    }
 }
 
